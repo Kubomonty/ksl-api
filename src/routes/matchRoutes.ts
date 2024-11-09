5db37dd875295a71f3fd73bf48342b37c5b2944d
Jakub Perhac <jakub.perhac@gmail.com>
2024-11-09 20:26:30 +0100

    rename match route

--- src/routes/matchRoutes.ts
+++ src/routes/matchRoutes.ts
@@ -4,7 +4,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/matches', getMatchesPageReq);
+router.get('/match', getMatchesPageReq);
 // router.get('/matches/:id', getMatchByIdReq);
 // router.put('/matches/:id', authenticate, updateMatchByIdReq);
 router.post('/match', authenticate, createMatchReq);
