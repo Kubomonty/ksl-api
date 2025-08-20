37aaec4e9d0e2516a445fe493b94ee29837d6c3f
Jakub Perhac <jakub.perhac@gmail.com>
2025-08-20 11:56:48 +0200

    removed tls

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -22,9 +22,6 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
