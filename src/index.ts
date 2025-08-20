cf4b6b773ea9f2ac3ac6571307a5e501ef0e8381
Kubomonty <83630866+Kubomonty@users.noreply.github.com>
2025-08-20 08:37:14 +0200

    Feat/separate server (#5)
    CORS Origins

--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,13 @@ import teamRoutes from './routes/teamRoutes.js';
 
 dotenv.config();
 const port = process.env.PORT || 8080;
-
 const app = express();
 
-const allowedOrigins = [
-  'https://ksl-ui.vercel.app',
-  'https://localhost:3000',
-  'http://localhost:3000',
-];
-
-// Flexibilné CORS middleware
+const allowedOrigins = ['http://91.98.112.7', 'https://kesl.sk', 'https://www.kesl.sk'];
 app.use(cors({
   origin: (origin, callback) => {
-    console.log('Request origin:', origin); // for debug
-    if (!origin) return callback(null, true); // for curl / Postman
-    if (
-      allowedOrigins.includes(origin) ||
-      origin.startsWith('http://localhost') ||
-      origin.startsWith('https://localhost') ||
-      origin.startsWith('http://91.98.112.7') ||
-      origin.startsWith('https://91.98.112.7')
-    ) {
+    console.log('Request origin:', origin);
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -41,12 +27,14 @@ app.use(cors({
 
 app.use(express.json());
 
+// API routy
 app.use('/api', matchRoutes);
 app.use('/api', playerRoutes);
 app.use('/api', seasonRoutes);
 app.use('/api', teamRoutes);
 app.use('/auth', authRoutes);
 
+// Protected route
 app.use('/api/protected', authenticate, (req, res) => {
   res.send('This is a protected route');
 });
@@ -57,6 +45,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send('Something broke!');
 });
 
+// Spustenie servera
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
